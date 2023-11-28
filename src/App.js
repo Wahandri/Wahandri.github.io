ae19a9a1895cf76897c33a7cc7a871d766c5397a
Tu Nombre <wahandri@wahandri-VirtualBox>
2023-11-28 10:39:04 +0100

    testeando

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Start from './components/Start/Start';
+import NumberGames from './components/NumberGames/NumberGames';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <div className='paddingApp'>
+          <Routes>
+            <Route path="/" element={<Start />} />
+            <Route path="/findTheNumber" element={<NumberGames />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
